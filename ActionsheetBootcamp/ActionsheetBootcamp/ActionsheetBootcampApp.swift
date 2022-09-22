be2877b7ac20609f68f855482b90d9e72e975e42
Bishowjit-Ray <82141518+Bishowjit-Ray@users.noreply.github.com>
2022-09-22 09:52:49 +0600

    Add files via upload

---
+++ ActionsheetBootcamp/ActionsheetBootcamp/ActionsheetBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActionsheetBootcampApp.swift
+//  ActionsheetBootcamp
+//
+//  Created by Bishowjit Ray on 16/9/22.
+//
+
+import SwiftUI
+
+@main
+struct ActionsheetBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
